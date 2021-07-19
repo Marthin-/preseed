36c92344453a22825ab1517adc4131d718d7d61c
Martin Guilloux <martin.guilloux@univ-brest.fr>
2021-07-19 15:29:37 +0200

    update pool name

--- deploy_debian.sh
+++ deploy_debian.sh
@@ -8,8 +8,8 @@ else
     hostname=debian
     cpu=1
     memory=4096
-    disk_size=32
-    disk_format=qcow2
+    disk_size=20
+    disk_format=raw
     net_bridge=br0
     location=http://ftp.debian.org/debian/dists/stable/main/installer-amd64/
     preseed_file=/tmp/preseed.cfg
@@ -17,8 +17,9 @@ else
     mask=255.255.255.0
     gw=192.168.1.1
     dns=8.8.8.8
-    disk_name=/dev/vda
+    disk_name=/dev/sda
     proxy=""
+    pool=STORE_PROD5
     [ -f $1 ] || { echo "preseed_vars file does not exist!" ; exit 1; }
     source $1
 
@@ -34,7 +35,7 @@ else
     sed -i "s:DISK_NAME:$disk_name:" $preseed_file
     sed -i "s:PROXY:$proxy:" $preseed_file
 
-    OPTIONS="--virt-type=kvm --name=$hostname --vcpus=$cpu --memory=$memory --os-variant=linux --disk size=$disk_size,format=$disk_format --network bridge=$net_bridge --console pty,target_type=serial --noautoconsole --location=$location --initrd-inject=$preseed_file -x 'auto=false,text,console=ttyS0,115200n8,serial'"
+    OPTIONS="--virt-type=kvm --name=$hostname --vcpus=$cpu --memory=$memory --os-variant=linux --disk pool=$pool,size=$disk_size,format=$disk_format --network bridge=$net_bridge --console pty,target_type=serial --noautoconsole --location=$location --initrd-inject=$preseed_file -x 'auto=false,text,console=ttyS0,115200n8,serial'"
 
     virt-install $OPTIONS --noreboot
     rm $preseed_file
